71324832bc4ad57c28cc0bb49679dc4e45579e83
Bernard Tison <bernard.tison@gmail.com>
2015-09-29 22:59:51 +0200

    added local temperature

--- www/weather-client.js
+++ www/weather-client.js
@@ -1,19 +1,59 @@
 document.getElementById('search_weather').onclick = function () {
   document.getElementById('cloudResponse').innerHTML = "<p>Calling App.....</p>";
+
+  var theCity = document.getElementById('the_city').value;
+
   $fh.cloud(
       {
-        path: 'weather',
+        path: 'weather/city',
         method: "POST",   //all other HTTP methods are supported as well. e.g. HEAD, DELETE, OPTIONS
         data: {
-          city: document.getElementById('the_city').value,
+          city: theCity,
           country: document.getElementById('the_country').value
         }
       },
       function (res) {
-        document.getElementById('cloudResponse').innerHTML = "<p>Temperature is " + res.msg + "</p>";
+        document.getElementById('cloudResponse').innerHTML = "<p>Temperature in " + theCity + " is " + res.msg + "</p>";
       },
       function (code, errorprops, params) {
         alert('An error occured: ' + code + ' : ' + errorprops);
       }
   );
+};
+
+document.getElementById('search_weather_by_gps_loc').onclick = function() {
+
+  var onSuccess = function(position) {
+    document.getElementById('cloudResponse').innerHTML = "<p>Calling App.....</p>";
+
+    $fh.cloud(
+      {
+        path: 'weather/location',
+        method: "POST",   //all other HTTP methods are supported as well. e.g. HEAD, DELETE, OPTIONS
+        data: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      },
+      function (res) {
+        document.getElementById('cloudResponse').innerHTML = "<p>Local Temperature is " + res.msg + " Celsius</p>";
+      },
+      function (code, errorprops, params) {
+        alert('An error occured: ' + code + ' : ' + errorprops);
+      }
+  );
+    
+
+  };
+
+  var onError = function(error) {
+    document.getElementById('cloudResponse').innerHTML = "<p>Error on getting location: " + error.message + " Celsius</p>";
+  };
+
+  if(typeof navigator.geolocation !== "undefined") {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    document.getElementById('cloudResponse').innerHTML = "<p>Geo Location not available</p>";
+  }
+   
 };
